7c5d56f9562797b304a569f70f2af0bf5d51b748
okikukawa <kikukawa0131@gmail.com>
2020-11-12 14:30:44 +0900

    テスト機能(Rspec)を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_31_071806) do
+ActiveRecord::Schema.define(version: 2020_11_12_023410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2020_10_31_071806) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "production_year"
+    t.integer "running_time"
+    t.text "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
